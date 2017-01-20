f8f77b73baf0255e828be33b4d7815faec1b63cd
javadcpp <javadcpp@gmail.com>
2017-01-20 19:44:23 +0330

    T1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" href="css/style.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- Load Bootstrap RTL theme from RawGit -->
+<link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
+</head>
+
+<body>
+<nav>
+	<div class="container-fluid" >
+    	<div class="row">
+        	<div class="col-lg-12">		
+             <center>			 
+        		<div class="contact">
+            	<div class="menu-bar">
+                <a href="Javascript:void(0)" class="tablink" onclick="openLink(event,'fade')" >معرفی موسسه</a>
+                 <a href="Javascript:void(0)" class="tablink" onclick="openLink(event,'fade')" >معرفی موسسه</a>
+                  <a href="Javascript:void(0)" class="tablink" onclick="openLink(event,'fade')" >معرفی موسسه</a>
+                   <a href="Javascript:void(0)" class="tablink" onclick="openLink(event,'fade')" >معرفی موسسه</a>
+                </div>
+            	</div>
+                </center>
+            </div>
+        </div>
+    </div>
+</nav>
+</body>
+</html>
